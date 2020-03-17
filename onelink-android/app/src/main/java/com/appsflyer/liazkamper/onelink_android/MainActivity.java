955d1d1926f1514b7b124fc9d02429d330d8ae2d
Dmytro Vasylkov <dmytro.vasylkov@appsflyer.com>
2020-03-17 20:57:10 +0200

    Android - working version after Dima fixes.

--- onelink-android/app/src/main/java/com/appsflyer/liazkamper/onelink_android/MainActivity.java
+++ onelink-android/app/src/main/java/com/appsflyer/liazkamper/onelink_android/MainActivity.java
@@ -1,68 +1,60 @@
 package com.appsflyer.liazkamper.onelink_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import com.appsflyer.AppsFlyerConversionListener;
 import com.appsflyer.AppsFlyerLib;
 
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String AF_DEV_KEY = "dQnXTbxz7UyXL7sy2rvgx";;
+import static com.appsflyer.liazkamper.onelink_android.AppsflyerShopApp.LOG_TAG;
 
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /** Set Up Conversion Listener to get attribution data **/
-        final NavController navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
-
-        AppsFlyerConversionListener conversionListener = new AppsFlyerConversionListener() {
-
-
+        AppsFlyerLib.getInstance().registerConversionListener(this, new AppsFlyerConversionListener() {
             @Override
             public void onConversionDataSuccess(Map<String, Object> conversionData) {
-
-                for (String attrName : conversionData.keySet()) {
-                    Log.d("AppsFlyer_LOG_TAG", "attribute: " + attrName + " = " + conversionData.get(attrName));
-
-                }
+                for (String attrName : conversionData.keySet())
+                    Log.d(LOG_TAG, "attribute: " + attrName + " = " + conversionData.get(attrName));
             }
 
             @Override
             public void onConversionDataFail(String errorMessage) {
-                Log.d("AppsFlyer_LOG_TAG", "error getting conversion data: " + errorMessage);
+                Log.d(LOG_TAG, "error getting conversion data: " + errorMessage);
             }
 
             @Override
             public void onAppOpenAttribution(Map<String, String> conversionData) {
-
-                for (String attrName : conversionData.keySet()) {
-                    Log.d("AppsFlyer_LOG_TAG", "attribute: " + attrName + " = " + conversionData.get(attrName));
-                }
-
-                Log.d("AppsFlyer_LOG_TAG","Deep linking into " + conversionData.get("af_sub1"));
+                for (String attrName : conversionData.keySet())
+                    Log.d(LOG_TAG, "attribute: " + attrName + " = " + conversionData.get(attrName));
+                Log.d(LOG_TAG, "Deep linking into " + conversionData.get("af_sub1"));
                 String targetSectionTag = conversionData.get("af_sub1") + "_section_fragment";
                 int targetId = getResources().getIdentifier(targetSectionTag, "id", getPackageName());
-
                 Bundle dp_args = new Bundle();
                 dp_args.putString("item_id", conversionData.get("item_id"));
-
-                PendingIntent pendingIntent = navController.createDeepLink().
-                                                setGraph(R.navigation.nav_graph_deeplink).
-                                                setDestination(targetId).
-                                                setArguments(dp_args).
-                                                createPendingIntent();
                 try {
-                    pendingIntent.send();
+                    Navigation.findNavController(MainActivity.this, R.id.my_nav_host_fragment)
+                            .createDeepLink()
+                            .setGraph(R.navigation.nav_graph_deeplink)
+                            .setDestination(targetId)
+                            .setArguments(dp_args)
+                            .createPendingIntent()
+                            .send();
                     finish();
                 } catch (PendingIntent.CanceledException e) {
                     e.printStackTrace();
@@ -71,10 +63,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAttributionFailure(String errorMessage) {
-                Log.d("AppsFlyer_LOG_TAG", "error onAttributionFailure : " + errorMessage);
+                Log.d(LOG_TAG, "error onAttributionFailure : " + errorMessage);
             }
-        };
-
-        AppsFlyerLib.getInstance().init(AF_DEV_KEY, conversionListener, this);
+        });
     }
 }
